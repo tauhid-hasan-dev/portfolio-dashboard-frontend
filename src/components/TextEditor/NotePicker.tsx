6ad93f661046fdbe8e6679555d301b59e5d44bf2
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-20 13:40:11 +0300

    text editor added

--- src/components/TextEditor/NotePicker.tsx
+++ src/components/TextEditor/NotePicker.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+"use client";
 
-const NotePicker = () => {
-  return <div>NotePicker</div>;
+import React, { useState } from "react";
+import Tiptap from "./Tiptap";
+
+const Todo = () => {
+  const [content, setContent] = useState<string>("");
+  const handleContentChange = (reason: any) => {
+    setContent(reason);
+  };
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const data = {
+      content: content,
+    };
+    console.log(data);
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="max-w-3xl w-full grid place-items-center mx-auto pt-10 mb-10"
+    >
+      <div className="text-3xl text-center text-black-500 mb-10">
+        Notes Picker
+      </div>
+      <Tiptap
+        content={content}
+        onChange={(newContent: string) => handleContentChange(newContent)}
+      />
+    </form>
+  );
 };
 
-export default NotePicker;
+export default Todo;
